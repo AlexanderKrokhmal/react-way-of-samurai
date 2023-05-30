1eec32c8a1a72172a235a6ff91fcb92bd1038a9b
Alexander <alexander.krokhmal.dev@gmail.com>
2023-05-30 20:12:24 +0300

    Add alt for image in Profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-
 function Profile() {
     return (
       <div className="content">
       <div>
-        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+            alt="Background"/>
       </div>
       <div>
         ava + description
