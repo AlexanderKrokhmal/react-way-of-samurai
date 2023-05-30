414fd5fc5066cb9825521259793bf8052e7937fe
Alexander <alexander.krokhmal.dev@gmail.com>
2023-05-30 19:20:27 +0300

    First init

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header className="header">
+        <img src=' https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/833px-Apple_logo_black.svg.png '/>
+      </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function Profile() {
+    return (
+      <div className="content">
+      <div>
+        <img src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
+      </div>
+      <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New posts
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 24
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+
+export default Profile;
